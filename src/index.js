362314609aa5342bb0beb4adbb9dab1f01849140
mitchell-mccracken <mpm5069@gmail.com>
2021-10-13 10:24:03 -0400

    changed procfile to reference App_copy and updated name of App_copy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import App from './App copy'
+import App from './App_copy'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
